c7719436e2df1fcc9650a38f3e17b752049fff15
ricky <rickyrojas@me.com>
2018-12-27 14:07:04 -0500

    inital

---
+++ schema.js
@@ -0,0 +1,94 @@
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
+} = require("graphql");
+const axios = require("axios");
+// Launch Type
+const LaunchType = new GraphQLObjectType({
+  name: "launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
+  })
+});
+
+// Rocket Type
+const RocketType = new GraphQLObjectType({
+  name: "rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+// User Type
+const UserType = new GraphQLObjectType({
+  name: "user",
+  fields: () => ({
+    name: { type: GraphQLString },
+    username: { type: GraphQLString },
+    email: { type: GraphQLString }
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches/")
+          .then(res => res.data);
+      }
+    },
+    launch: {
+      type: LaunchType,
+      args: { flight_number: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then(res => res.data);
+      }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/rockets/")
+          .then(res => res.data);
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parent, args) {
+        return axios
+          .get("https://jsonplaceholder.typicode.com/users")
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
